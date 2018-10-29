e862994899a3d25e48659eed8db6f0ff8ff5001f
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-10-29 15:37:59 +0500

    Rename lists to have lists word in name

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
 
 # will add more keywords as they're implemented
-temporal_keywords = ["during"]
+temporal_keywords_list = ["during"]
+temporal_predicates_list = ["during", "after"]
--- app/parser/syntax_constraints_check.py
+++ app/parser/syntax_constraints_check.py
@@ -1,17 +1,28 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
+from app.constants import temporal_keywords_list
 
 
 class SyntaxConstraintsCheck:
+    """
+    For more information about these checks, see
+    3. Preliminaries -> 1. Definition -> syntax constraints in
+    https://dl.comp.nus.edu.sg/bitstream/handle/1900.100/6906/TRA3_18.pdf
+    """
+
     def __init__(self, temporal_keywords):
 
         self.temporal_keywords = temporal_keywords
+
         self.check_keywords(temporal_keywords)
         self.check_time_period_adjacent_to_temporal_predicate(
             temporal_keywords)
         self.check_temporal_predicate(temporal_keywords)
 
     def check_keywords(self, temporal_keywords):
-        pass
+        for word in temporal_keywords_list:
+            if word in temporal_keywords[0] and word in temporal_keywords[-1]:
+                raise Exception(
+                    "Temporal predicate cannot be in the start or end.")
 
     def check_time_period_adjacent_to_temporal_predicate(
             self, temporal_keywords):
