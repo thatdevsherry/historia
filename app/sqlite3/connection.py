77e6191c5248efe818902c3c2140e2416495687d
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-10-30 11:11:55 +0500

    Make connection's  execute method static

--- app/sqlite3/connection.py
+++ app/sqlite3/connection.py
@@ -20,9 +20,10 @@ class Connection:
         temporal_database = "temporal_" + self.database_file
         return temporal_database
 
+    @staticmethod
     def execute(args):
         parsed_query = QueryParser(args)
         basic_keywords = parsed_query.get_basic_keywords()
         temporal_keywords = parsed_query.get_temporal_keywords()
         SelectQuery.perform_query_action(basic_keywords, temporal_keywords)
-        return parsed_query  # for testing purposes
+        return parsed_query
