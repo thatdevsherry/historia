7c5e45cb956e3e469f3c762486b0e222f6248477
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-10-30 10:11:45 +0500

    Add empty functions for SelectQuery class

--- app/query_action/select.py
+++ app/query_action/select.py
@@ -1,5 +1,14 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
+import sqlite3
 
 
 class SelectQuery:
-    pass
+    def perform_query_action(basic_keywords, temporal_keywords):
+        SelectQuery.perform_basic_keywords_action(basic_keywords)
+        SelectQuery.perform_temporal_keywords_action(temporal_keywords)
+
+    def perform_basic_keywords_action(basic_keywords):
+        pass
+
+    def perform_temporal_keywords_action(temporal_keywords):
+        pass
