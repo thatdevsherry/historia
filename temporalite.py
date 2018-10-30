f5bd9b3fb70a094f0112e2fd1d701127510371d9
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-10-30 10:11:20 +0500

    Move execution function to Connection class
    execution method has been moved to Connection object as it should
    only work when there is a connection object.
    
    Added a connect method in main module to first create a connection
    and then ask dem queries.

--- temporalite.py
+++ temporalite.py
@@ -1,13 +1,6 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
-from app.parser.query_parser import QueryParser
+from app.sqlite3.connection import Connection
 
 
-def main_execution(args):
-    parsed_query = QueryParser(args)
-    basic_keywords = parsed_query.get_basic_keywords()
-    temporal_keywords = parsed_query.get_temporal_keywords()
-    return parsed_query  # for testing purposes
-
-
-if "__name__" == "__main__":
-    main_execution(*args)
+def connect(file_name):
+    return Connection(file_name)
