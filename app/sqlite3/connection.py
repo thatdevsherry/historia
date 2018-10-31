714a07ca71729870266ff4b57a201bbbe9704127
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-10-31 15:42:50 +0500

    Connection class now has attribute connection
    The connection is passed to handlers of queries which use it for
    executing them.

--- app/sqlite3/connection.py
+++ app/sqlite3/connection.py
@@ -1,23 +1,19 @@
 import sqlite3
 
 from app.parser.query_parser import QueryParser
-from app.intercept.query_info import QueryActionInfo
+from app.intercept.query_info import QueryHandler
 
 
 class Connection:
-    def __init__(self, database_file):
+    def __init__(self, database_file, connection=None):
+        self.connection = connection
         self.database_file = database_file
         self.verify_file_path()
 
     def verify_file_path(self):
         # raises sqlite3's exception if file path not valid
-        sqlite3.connect(self.database_file)
+        self.connection = sqlite3.connect(self.database_file)
 
     def execute(self, args):
         parsed_query = QueryParser(args)
-        query_info = QueryActionInfo.decide_action(parsed_query)
-        # perform sqlite query
-        connection = sqlite3.connect(self.database_file)
-        connection.execute(' '.join(query_info.query.query))
-        # perform temporal query
-        connection.execute(query_info.temporal_query)
+        return QueryHandler.action_handler(self.connection, parsed_query)
