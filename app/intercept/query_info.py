3d2f93dea99e357e011a8b76369748903bf4ea1a
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-10-31 15:41:43 +0500

    Add handlers for create and insert query
    calls methods that are responsible for doing their stuff.
    Noice commit msg

--- app/intercept/query_info.py
+++ app/intercept/query_info.py
@@ -1,8 +1,16 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
-from app.intercept.create import CreateTableQuery
+from app.intercept.create import CreateQueryBuilder
+from app.intercept.insert import InsertQueryBuilder
+from app.sqlite3.query_execution.create import CreateQuery
+from app.sqlite3.query_execution.insert import InsertQuery
 
 
-class QueryActionInfo:
-    def decide_action(parsed_query):
+class QueryHandler:
+    def action_handler(connection, parsed_query):
         if parsed_query.query[0] == "create":
-            return CreateTableQuery(parsed_query)
+            query_info = CreateQueryBuilder(parsed_query)
+            CreateQuery.execute(connection, query_info)
+
+        elif parsed_query.query[0] == "insert":
+            query_info = InsertQueryBuilder(parsed_query)
+            InsertQuery.execute(connection, query_info)
