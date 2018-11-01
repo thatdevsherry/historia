12e88863210237e3606bf3259a53436b1c9100f0
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-01 15:18:16 +0500

    Add DeleteQuery class
    This class is responsible for executing the queries created by
    DeleteQueryBuilder class.

---
+++ app/sqlite3/query_execution/delete.py
@@ -0,0 +1,9 @@
+# Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
+
+
+class DeleteQuery:
+    def execute(connection, query_info):
+        # execute normal query
+        connection.execute(' '.join(query_info.query.query))
+        # execute temporal table query
+        connection.execute(query_info.temporal_query)
