fcaa6e0d0d55a617cce4921e3cdc565969d2b3c1
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-01 14:18:28 +0500

    Add handler for when entered query has update word

--- app/intercept/query_info.py
+++ app/intercept/query_info.py
@@ -1,8 +1,10 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
 from app.intercept.create import CreateQueryBuilder
 from app.intercept.insert import InsertQueryBuilder
+from app.intercept.update import UpdateQueryBuilder
 from app.sqlite3.query_execution.create import CreateQuery
 from app.sqlite3.query_execution.insert import InsertQuery
+from app.sqlite3.query_execution.update import UpdateQuery
 
 
 class QueryHandler:
@@ -14,3 +16,7 @@ class QueryHandler:
         elif parsed_query.query[0] == "insert":
             query_info = InsertQueryBuilder(parsed_query)
             InsertQuery.execute(connection, query_info)
+
+        elif parsed_query.query[0] == "update":
+            query_info = UpdateQueryBuilder(parsed_query, connection)
+            UpdateQuery.execute(connection, query_info)
