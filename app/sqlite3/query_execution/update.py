042449bf3625198d290d6869af6c38b13a17032d
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-01 14:22:20 +0500

    Add UpdateQuery class which runs the built queries
    Runs queries built by UpdateQueryBuilder class.

---
+++ app/sqlite3/query_execution/update.py
@@ -0,0 +1,11 @@
+# Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
+
+
+class UpdateQuery:
+    def execute(connection, query_info):
+        # execute user_query
+        connection.execute(' '.join(query_info.query.query))
+        # update temporal row
+        connection.execute(query_info.temporal_query)
+        # insert new row in temporal table
+        connection.execute(query_info.temporal_query_insert)
