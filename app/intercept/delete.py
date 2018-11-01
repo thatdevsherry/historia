5de38395a6115fa40e5a23e135668b608f30e205
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-01 15:17:49 +0500

    Add DeleteQueryBuilder class
    The class is responsible for creating queries that will be
    executed when user enters a query with "delete" keyword in it.
    It changes the valid_to column in temporal database from null to
    date on which the row in normal table was deleted.

---
+++ app/intercept/delete.py
@@ -0,0 +1,67 @@
+# Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
+import time
+
+from pudb import set_trace
+
+
+class DeleteQueryBuilder:
+    def __init__(self,
+                 query,
+                 temporal_query=None,
+                 table_name=None,
+                 temporal_table_name=None,
+                 reference_column_name=None,
+                 reference_columnd_value=None):
+        self.query = query
+        self.temporal_query = temporal_query
+        self.table_name = table_name
+        self.temporal_table_name = temporal_table_name
+
+        self.set_table_names()
+        self.build_temporal_query()
+
+    def set_table_names(self):
+        DeleteQueryBuilder.set_original_table_name(self)
+        DeleteQueryBuilder.set_temporal_table_name(self)
+
+    def set_original_table_name(self):
+        set_trace()
+        original_query = self.query.query
+
+        for word in original_query:
+            if word == "from":
+                from_keyword_index = original_query.index(word)
+                break
+            else:
+                pass
+
+        table_name = original_query[from_keyword_index + 1]
+        self.table_name = table_name
+
+    def set_temporal_table_name(self):
+        self.temporal_table_name = self.table_name + "_temporal"
+
+    def build_temporal_query(self):
+        original_query = self.query.query
+
+        for word in original_query:
+            if word == "where":
+                where_keyword_index = original_query.index(word)
+                break
+            else:
+                pass
+
+        set_trace()
+        reference_keywords = original_query[where_keyword_index + 1]
+
+        # get current time
+        day = time.localtime()[2]
+        month = time.localtime()[1]
+        year = time.localtime()[0]
+
+        time_string = "{}-{}-{}".format(day, month, year)
+
+        temporal_query = "update {} set valid_to='{}' where {}".format(
+            self.temporal_table_name, time_string, reference_keywords)
+
+        self.temporal_query = temporal_query
