3f9ec79cc2bf05b3b6fe717a18e7943e97c4d229
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-02 10:30:14 +0500

    rename attribute to make more sense of what it is

--- app/sqlite3/connection.py
+++ app/sqlite3/connection.py
@@ -1,25 +1,26 @@
 import sqlite3
 
 from app.parser.query_parser import QueryParser
-from app.intercept.query_info import QueryHandler
+from app.intercept.query_handler import QueryHandler
 
 
 class Connection:
-    def __init__(self, database_file, connection=None):
-        self.connection = connection
+    def __init__(self, database_file, sqlite_connection=None):
+        self.sqlite_connection = sqlite_connection
         self.database_file = database_file
         self.verify_file_path()
 
     def verify_file_path(self):
         # raises sqlite3's exception if file path not valid
-        self.connection = sqlite3.connect(self.database_file)
+        self.sqlite_connection = sqlite3.connect(self.database_file)
 
     def execute(self, args):
         parsed_query = QueryParser(args)
-        return QueryHandler.action_handler(self.connection, parsed_query)
+        return QueryHandler.action_handler(self.sqlite_connection,
+                                           parsed_query)
 
     def commit(self):
-        self.connection.commit()
+        self.sqlite_connection.commit()
 
     def close(self):
-        self.connection.close()
+        self.sqlite_connection.close()
