4314915622f8bc2d446068238b031c46f91ab53d
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-05 14:27:18 +0500

    Use regex to see if select query is temporal

--- app/intercept/select_handler.py
+++ app/intercept/select_handler.py
@@ -1,11 +1,27 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
-from app.constants import select_temporal_keywords
+import re
+
+from pudb import set_trace
 
 
 class SelectQueryHandler:
     def is_temporal_query(parsed_query):
-        for word in parsed_query:
-            if word in select_temporal_keywords:
-                return True
+        set_trace()
+        original_query = ' '.join(parsed_query)
 
-        return False
+        # will add more clauses as they're implemented
+        temporal_clause_pattern = re.compile(r'(as of )')
+
+        temporal_clause_matches = temporal_clause_pattern.finditer(
+            original_query)
+
+        temporal_clause_match = None
+
+        for match in temporal_clause_matches:
+            temporal_clause_match = match
+
+        if temporal_clause_match is not None:
+            return True
+
+        else:
+            return False
