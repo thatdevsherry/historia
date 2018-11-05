d0b1f2bc4b2b6d6a971bed9653fa540f26daaab6
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-05 14:09:38 +0500

    Mamamia UpdateQueryBuilder now uses regex
    Luigi is disappointed. He made the parsing work manually by
    converting it to list and then using whole lotta if statements but
    mario is a smart plumber. He replaced the parsing with regex.

--- app/intercept/update.py
+++ app/intercept/update.py
@@ -1,5 +1,5 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
-import sqlite3
+import re
 import datetime
 
 
@@ -28,14 +28,16 @@ class UpdateQueryBuilder:
         self.build_queries()
 
     def set_original_table_name(self):
-        original_query = self.query.query
-        for word in original_query:
-            if word == "update":
-                table_name_index = original_query.index(word) + 1
-                self.table_name = original_query[table_name_index]
-                break
-            else:
-                pass
+        original_query = ' '.join(self.query.query)
+
+        table_name_pattern = re.compile(r'(?<=update )[^ ]+')
+
+        table_name_matches = table_name_pattern.finditer(original_query)
+
+        for match in table_name_matches:
+            table_name_match = match
+
+        self.table_name = table_name_match.group(0)
 
     def set_temporal_table_name(self):
         table_name = self.table_name
@@ -46,73 +48,85 @@ class UpdateQueryBuilder:
         UpdateQueryBuilder.set_temporal_table_name(self)
 
     def build_queries(self):
-        UpdateQueryBuilder.build_temporal_query(self)
-        UpdateQueryBuilder.build_temporal_query_insert(self)
-
-    def build_temporal_query(self):
-        original_query = self.query.query
-
-        for word in original_query:
-            if word == "where":
-                where_keyword_index = original_query.index(word)
-                break
-            else:
-                pass
-
-        # we assume user entered column_name=value (without spaces) for now
-
-        # TODO: see if user entered column_name=value or without spaces
-        # and then parse correctly
-
-        reference_column = original_query[where_keyword_index + 1]
-        split_word = reference_column.split('=')
-        column_name = split_word[0]
-        self.reference_column_name = column_name
-        column_value = split_word[1]
-        self.reference_column_value = column_value
-
         date_string = datetime.datetime.now().isoformat()
+        UpdateQueryBuilder.build_temporal_query(self, date_string)
+        UpdateQueryBuilder.build_temporal_query_insert(self, date_string)
 
-        temporal_query = "update {} set valid_to='{}' where {}={}".format(
-            self.temporal_table_name, date_string, column_name, column_value)
+    def build_temporal_query(self, date_string):
+        original_query = ' '.join(self.query.query)
+
+        condition_pattern = re.compile(r'(?<=where )[^ ]+')
+
+        condition_matches = condition_pattern.finditer(original_query)
+
+        for match in condition_matches:
+            condition_match = match
+
+        condition = condition_match.group(0)
+
+        temporal_query = "update {} set valid_to='{}' where {} and valid_to='9999-12-31T00:00:00.000000'".format(
+            self.temporal_table_name, date_string, condition)
 
         self.temporal_query = temporal_query
 
-    def build_temporal_query_insert(self):
-        original_query = self.query.query
-        for word in original_query:
-            if word == "set":
-                set_keyword_index = original_query.index(word)
-            else:
-                pass
+    def build_temporal_query_insert(self, date_string):
+        original_query = ' '.join(self.query.query)
 
-        change_value_word = original_query[set_keyword_index + 1]
-        split_word = change_value_word.split('=')
-        column_name = split_word[0]
-        new_value = split_word[1].strip("'")
+        set_value_pattern = re.compile(r'(?<=set )[^ ]+')
+
+        set_value_match = set_value_pattern.finditer(original_query)
+
+        for match in set_value_match:
+            set_value_match = match
+
+        set_value_string = set_value_match.group(0)
+
+        new_value_pattern = re.compile(r'(?<==)[^ ]+')
+
+        new_value_matches = new_value_pattern.finditer(set_value_string)
+
+        for match in new_value_matches:
+            new_value_match = match
+
+        new_value_unstripped = new_value_match.group(0)
+        new_value = new_value_unstripped.strip("'")
+
+        where_clause_value_pattern = re.compile(r'(?<=where )[^ ]+')
+
+        where_clause_value_matches = where_clause_value_pattern.finditer(
+            original_query)
+
+        for match in where_clause_value_matches:
+            where_clause_value_match = match
+
+        where_clause_value = where_clause_value_match.group(0)
+
+        update_column_pattern = re.compile(r'(?<=set )[^=]+')
+
+        update_column_matches = update_column_pattern.finditer(original_query)
+
+        for match in update_column_matches:
+            update_column_match = match
+
+        update_column = update_column_match.group(0)
 
         # get full row
-        query = self.connection.execute(
-            "select * from {} where {} = {}".format(
-                self.table_name, self.reference_column_name,
-                self.reference_column_value))
+        query = self.connection.execute("select * from {} where {}".format(
+            self.table_name, where_clause_value))
         query_result = query.fetchone()
         self.row_tuple = query_result
 
-        get_name_query = self.connection.execute(
-            "select {} from {} where {} = {}".format(
-                column_name, self.table_name, self.reference_column_name,
-                self.reference_column_value))
+        get_column_query = self.connection.execute(
+            "select {} from {} where {}".format(update_column, self.table_name,
+                                                where_clause_value))
 
-        previous_value = get_name_query.fetchone()[0]
-
-        date_string = datetime.datetime.now().isoformat()
+        previous_value = get_column_query.fetchone()[0]
 
         query_result_list = list(query_result)
         old_value_index = query_result_list.index(previous_value)
         query_result_list.pop(old_value_index)
         query_result_list.insert(old_value_index, new_value)
-        query_result_list.append(date_string)
+        query_result_list.append('{}'.format(date_string))
         query_result_list.append('9999-12-31T00:00:00.000000')
         new_tuple = tuple(query_result_list)
 
