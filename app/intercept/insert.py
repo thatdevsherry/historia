9f1f28f07758adfc135f360f616bef069046c264
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-06 13:47:06 +0500

    Refactor InsertQueryBuilder class
    Add table/temporal table name attributes and split
    build_temporal_query function logic into multiple smaller
    functions for abstraction.

--- app/intercept/insert.py
+++ app/intercept/insert.py
@@ -4,16 +4,25 @@ import datetime
 
 
 class InsertQueryBuilder:
-    def __init__(self, query, temporal_query=None):
+    def __init__(self,
+                 query,
+                 table_name=None,
+                 temporal_table_name=None,
+                 temporal_query=None):
         self.query = query
+        self.table_name = table_name
+        self.temporal_table_name = temporal_table_name
         self.temporal_query = temporal_query
 
+        self.set_table_names()
         self.build_temporal_query()
 
-    def build_temporal_query(self):
-        original_query = ' '.join(self.query.query)
+    def set_table_names(self):
+        InsertQueryBuilder.set_original_table_name(self)
+        InsertQueryBuilder.set_temporal_table_name(self)
 
-        # get table name
+    def set_original_table_name(self):
+        original_query = ' '.join(self.query.query)
         table_name_pattern = re.compile(r'(?<=into )(.*)(?= values)')
 
         matches = table_name_pattern.finditer(original_query)
@@ -22,16 +31,26 @@ class InsertQueryBuilder:
             table_name_match = match
 
         table_name_span = table_name_match.span()
-        table_name = table_name_match.group(0)
+        self.table_name = table_name_match.group(0)
 
-        temporal_table = table_name + "_history"
+    def get_table_span(original_query):
+        table_name_pattern = re.compile(r'(?<=into )(.*)(?= values)')
 
-        # get string before table name
-        before_table = original_query[:table_name_span[0]]
+        matches = table_name_pattern.finditer(original_query)
 
+        for match in matches:
+            table_name_match = match
+
+        table_name_span = table_name_match.span()
+        return table_name_span
+
+    def set_temporal_table_name(self):
+        self.temporal_table_name = self.table_name + "_history"
+
+    def get_after_table_part(self, original_query):
         # get values b/w table and opening bracket '(' of values
-        keyword_values_pattern = re.compile(
-            r'(?<={})(.*)(?=\()'.format(table_name))
+        keyword_values_pattern = re.compile(r'(?<={})(.*)(?=\()'.format(
+            self.table_name))
 
         keyword_values_matches = keyword_values_pattern.finditer(
             original_query)
@@ -40,8 +59,9 @@ class InsertQueryBuilder:
             keyword_values_match = match
 
         after_table_before_values = keyword_values_match.group(0)
+        return after_table_before_values
 
-        # get values inside brackets
+    def get_values(original_query):
         values_pattern = re.compile(r'\((.*)(?=\))')
 
         values_matches = values_pattern.finditer(original_query)
@@ -50,6 +70,20 @@ class InsertQueryBuilder:
             values_match = match
 
         values = values_match.group(0)
+        return values
+
+    def build_temporal_query(self):
+        original_query = ' '.join(self.query.query)
+
+        table_name_span = InsertQueryBuilder.get_table_span(original_query)
+
+        # get string before table name
+        before_table = original_query[:table_name_span[0]]
+
+        after_table_before_values = InsertQueryBuilder.get_after_table_part(
+            self, original_query)
+
+        values = InsertQueryBuilder.get_values(original_query)
 
         time_string = datetime.datetime.now().isoformat()
 
@@ -58,5 +92,5 @@ class InsertQueryBuilder:
         add_values = "{}, '{}', '9999-12-31T00:00:00.000000')".format(
             values, time_string)
 
-        self.temporal_query = before_table + temporal_table + \
+        self.temporal_query = before_table + self.temporal_table_name + \
             after_table_before_values + add_values
