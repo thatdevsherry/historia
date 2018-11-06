bf16542473b4a71e909b5ef5555726f3ce9aa774
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-06 11:38:57 +0500

    Refactor CreateQueryBuilder.build_temporal_query()
    Split down function logic into functions for abstraction.
    
    P.S Clean code.. looks nice.

--- app/intercept/create.py
+++ app/intercept/create.py
@@ -16,8 +16,11 @@ class CreateQueryBuilder:
         self.set_table_names()
         self.build_temporal_query()
 
+    def set_table_names(self):
+        CreateQueryBuilder.set_original_table_name(self)
+        CreateQueryBuilder.set_temporal_table_name(self)
+
     def set_original_table_name(self):
-        # convert query tuple to string
         original_query = ' '.join(self.query.query)
         get_table_pattern = re.compile(r'(?<=table )(.*)(?= \()')
 
@@ -32,40 +35,15 @@ class CreateQueryBuilder:
     def set_temporal_table_name(self):
         self.temporal_table_name = self.table_name + "_history"
 
-    def set_table_names(self):
-        CreateQueryBuilder.set_original_table_name(self)
-        CreateQueryBuilder.set_temporal_table_name(self)
-
     def build_temporal_query(self):
-        # convert query tuple to string
         original_query = ' '.join(self.query.query)
-        get_columns_pattern = re.compile(r'\([^)]+\w')
 
-        matches = get_columns_pattern.finditer(original_query)
+        new_columns_string = CreateQueryBuilder.build_temporal_columns(
+            original_query)
 
-        for match in matches:
-            columns = match.group(0)
+        table_part = CreateQueryBuilder.get_table_part(original_query)
 
-        new_columns_string = columns + ", valid_from datetime, valid_to datetime)"
-
-        get_table_pattern = re.compile(r'(?<=table )(.*)(?= \()')
-
-        another_match = get_table_pattern.finditer(original_query)
-
-        for match in another_match:
-            table_names = match
-
-        table_name_span = table_names.span()
-        table_part = original_query[:table_name_span[0]]
-
-        primary_key_pattern = re.compile(r' primary key')
-
-        primary_keys = primary_key_pattern.finditer(new_columns_string)
-
-        key_span = None
-
-        for match in primary_keys:
-            key_span = match.span()
+        key_span = CreateQueryBuilder.without_primary_key(new_columns_string)
 
         if key_span is not None:
 
@@ -77,3 +55,38 @@ class CreateQueryBuilder:
         else:
             self.temporal_query = table_part + \
                 self.temporal_table_name + " " + new_columns_string
+
+    def build_temporal_columns(original_query):
+        get_columns_pattern = re.compile(r'\([^)]+\w')
+
+        matches = get_columns_pattern.finditer(original_query)
+
+        for match in matches:
+            columns = match.group(0)
+
+        new_columns_string = columns + ", valid_from datetime, valid_to datetime)"
+        return new_columns_string
+
+    def get_table_part(original_query):
+        get_table_pattern = re.compile(r'(?<=table )(.*)(?= \()')
+
+        another_match = get_table_pattern.finditer(original_query)
+
+        for match in another_match:
+            table_names = match
+
+        table_name_span = table_names.span()
+        table_part = original_query[:table_name_span[0]]
+        return table_part
+
+    def without_primary_key(new_columns_string):
+        primary_key_pattern = re.compile(r' primary key')
+
+        primary_keys = primary_key_pattern.finditer(new_columns_string)
+
+        key_span = None
+
+        for match in primary_keys:
+            key_span = match.span()
+
+        return key_span
