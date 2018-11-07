93df437427c0853d1d39669d51fa09345bd34ed5
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-07 10:55:14 +0500

    Add some fields to setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@ from distutils.core import setup
 
 setup(
     name='temporalite',
-    version = '0.1'
+    version='0.1',
+    download_url=
+    'https://github.com/ShehriyarQureshi/temporalite/archive/0.1.tar.gz',
+    keywords=[
+        'temporal', 'database', 'tables', 'history', 'history-table',
+        'temporal-table'
+    ],
     description='Implement temporal tables using sqlite3 module',
     url='https://www.github.com/ShehriyarQureshi/temporalite.git',
     author='Shehriyar Qureshi',
     author_email='SShehriyar266@gmail.com',
     license='GPLv3',
     packages=[
-        'temporalite',
-        'temporalite.connection',
-        'temporalite.constants',
-        'temporalite.intercept',
-        'temporalite.parser',
-        'temporalite.sqlite3',
+        'temporalite', 'temporalite.connection', 'temporalite.constants',
+        'temporalite.intercept', 'temporalite.parser', 'temporalite.sqlite3'
     ],
     zip_safe=False,
 )
