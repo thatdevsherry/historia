22e9d5bda844d409aa4d302734a59ce0723d55c8
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-08 11:41:49 +0500

    Add tests for Connection class

---
+++ temporalite/tests/sqlite3/test_connection.py
@@ -0,0 +1,40 @@
+import sqlite3
+import subprocess
+
+from temporalite.sqlite3.connection import Connection
+
+import pytest
+
+
+def teardown_module():
+    subprocess.call(["rm", "test_file"])
+
+
+def test_connection_path():
+    test_connection = Connection('test_file')
+    assert test_connection.verify_file_path() is None
+
+
+def test_connection_file_name():
+    test_connection = Connection('test_file')
+    assert test_connection.database_file == 'test_file'
+
+
+def test_sqlite_connection():
+    test_connection = Connection('test_file')
+    assert test_connection.sqlite_connection is not None
+
+
+def test_invalid_path_to_database_file():
+    with pytest.raises(sqlite3.OperationalError):
+        Connection('in/valid/file.db')
+
+
+def test_connection_commit():
+    test_connection = Connection('test_file')
+    assert test_connection.commit() is None
+
+
+def test_connection_close():
+    test_connection = Connection('test_file')
+    assert test_connection.close() is None
