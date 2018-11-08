bbaed2a04090b2157914f676a243654a7b180233
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-11-08 13:41:27 +0500

    Change how connect method is called

---
+++ temporalite/connect.py
@@ -0,0 +1,5 @@
+from temporalite.connection.connection import Connection
+
+
+def connect(file_name):
+    return Connection(file_name)
--- temporalite/connection/temporalite.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
-from temporalite.sqlite3.connection import Connection
-
-
-def connect(file_name):
-    return Connection(file_name)
