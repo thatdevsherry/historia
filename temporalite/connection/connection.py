f5562bb43802e1174d31e3f1ca09a58a9fdf2b20
Shehriyar Qureshi <SShehriyar266@gmail.com>
2018-12-26 13:00:16 +0500

    Add function to automatically create history tables
    When executed, the function goes over all the tables and checks
    which ones don't have a history table. If found, it creates the
    history table for those tables.

--- temporalite/connection/connection.py
+++ temporalite/connection/connection.py
@@ -1,7 +1,9 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
 import sqlite3
 
+from temporalite.intercept.create import CreateQueryBuilder
 from temporalite.intercept.query_handler import QueryHandler
+from temporalite.query_execution.create import CreateQuery
 
 
 class Connection:
@@ -9,6 +11,7 @@ class Connection:
         self.sqlite_connection = sqlite_connection
         self.database_file = database_file
         self.verify_file_path()
+        self.create_history_tables()
 
     def verify_file_path(self):
         # raises sqlite3's exception if file path not valid
@@ -23,3 +26,35 @@ class Connection:
 
     def close(self):
         self.sqlite_connection.close()
+
+    # TODO: implement function to automatically create history tables of
+    # tables that already exist in db file
+
+    def create_history_tables(self):
+        query = self.sqlite_connection.execute(
+            "SELECT name FROM sqlite_master WHERE type='table'")
+        query_output = query.fetchall()
+        table_names = []
+        table_schemas = []
+
+        for i in query_output:
+            table_names.append(i[0])
+
+        for table in table_names:
+            # leave out already created history tables
+            if table[-8:] == '_history':
+                pass
+            else:
+                table_name = [table + "_history"]
+                query = self.sqlite_connection.execute(
+                    "select sql from sqlite_master where name=?", table_name)
+                query_output = query.fetchone()
+                # check if table has a history table
+                if query_output is None:
+                    query = self.sqlite_connection.execute(
+                        "select sql from sqlite_master where name=?", [table])
+                    table_schemas.append(str.lower(query.fetchone()[0]))
+
+        for i in table_schemas:
+            query_info = CreateQueryBuilder(i)
+            CreateQuery.execute(self.sqlite_connection, query_info)
--- temporalite/query_execution/create.py
+++ temporalite/query_execution/create.py
@@ -1,8 +1,12 @@
 # Copyright 2018 Shehriyar Qureshi <SShehriyar266@gmail.com>
+import sqlite3
 
 
 class CreateQuery:
     def execute(connection, query_info):
-        connection.execute(query_info.query)
+        try:
+            connection.execute(query_info.query)
+        except sqlite3.OperationalError:
+            pass
         # perform temporal query
         connection.execute(query_info.temporal_query)
